90f7bad28e5e13d927956e723f5b9a7081693231
mezan <mrmezan0@gmail.com>
2019-12-28 07:09:20 +0600

    Challenge-1 Done!!!

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage("https://www.w3schools.com/w3css/img_lights.jpg"),
+        ),
+      ),
+    ),
+  ));
+}
